63617cc06400e98ffcc3b9f6682602fadfeb5994
augustinh22 <hannah.augustin@gmail.com>
2025-08-11 15:54:50 +0200

    :fix: try to fix unterminated string

--- farmapp.py
+++ farmapp.py
@@ -174,9 +174,9 @@ def kulturname(kultur_name):
             for prio_bed in prio_beds_list:
                 if prio_bed in harvest_rest_list:
                     harvest_rest_list.remove(prio_bed)
-            priority_info = f"Ja! Hier zuerst ernten: <mark>{str(prio_beds_list).replace("[", '').replace("]", '').replace("'", '')}</mark>"
+            priority_info = f"""Ja! Hier zuerst ernten: <mark>{str(prio_beds_list).replace("[", '').replace("]", '').replace("'", '')}</mark>"""
             if len(harvest_rest_list) >= 1:
-                add_str = f"</br> und dann in dieser Reihenfolge weiterschauen: <mark>{str(harvest_rest_list).replace("[", '').replace("]", '').replace("'", '')}</mark>"
+                add_str = f"""</br> und dann in dieser Reihenfolge weiterschauen: <mark>{str(harvest_rest_list).replace("[", '').replace("]", '').replace("'", '')}</mark>"""
                 priority_info += add_str
 
     return render_template(
@@ -356,9 +356,9 @@ def ernteliste_table():
             if prio_bed in harvest_rest_list:
                 harvest_rest_list.remove(prio_bed)
         priority_info = str()
-        priority_info = f"<mark>{str(prio_beds_list).replace("[", '').replace("]", '').replace("'", '')}</mark>"
+        priority_info = f"""<mark>{str(prio_beds_list).replace("[", '').replace("]", '').replace("'", '')}</mark>"""
         if len(harvest_rest_list) >= 1:
-            add_str = f", <i><small>aber auch: <mark>{str(harvest_rest_list).replace("[", '').replace("]", '').replace("'", '')}</mark></small></i>"
+            add_str = f""", <i><small>aber auch: <mark>{str(harvest_rest_list).replace("[", '').replace("]", '').replace("'", '')}</mark></small></i>"""
             priority_info += add_str
         harvestable_dict[veggie] = priority_info
         harvest_text += f"</br>{veggie}: {priority_info}"
@@ -435,7 +435,7 @@ def empty_year_list_gen(min_right=0, max_right_plus_one=43, min_left=51, max_lef
     empty_year_list = []
     for x in range(min_right, max_right_plus_one):
         empty_year_list.append(str(x))
-    for x in range (min_left, max_left_plus_one):
+    for x in range(min_left, max_left_plus_one):
         empty_year_list.append(str(x))
     return empty_year_list
 
